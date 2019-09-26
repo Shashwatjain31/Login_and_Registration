a30d7a06535123cfdeadc5503c3682f70f27619e
Shashwat Jain <shashwatjain31@gmail.com>
2019-09-25 19:05:39 -0700

    Initial commit

---
+++ app/src/main/java/com/example/loginandregistration/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.loginandregistration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mTextUsername;
+    EditText mTextPassword;
+    Button mButtonLogin;
+    TextView mtextViewRegister;
+    DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db = new DatabaseHelper(this);
+        mTextUsername = (EditText)findViewById(R.id.edittext_username);
+        mTextPassword = (EditText)findViewById(R.id.edittext_password);
+        mButtonLogin = (Button)findViewById(R.id.button_login);
+        mtextViewRegister = (TextView)findViewById(R.id.textview_register);
+
+        mtextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = mTextUsername.getText().toString().trim();
+                String pwd = mTextPassword.getText().toString().trim();
+                Boolean res = db.checkUser(user, pwd);
+
+                if(res == true){
+                    Intent HomePage = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(HomePage);
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Login Error", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
